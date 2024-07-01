70aee6b1519f5065781698528cfb0a4caa9e8dcd
Roberto Amador <83928147+Chocoyito@users.noreply.github.com>
2024-06-30 22:51:10 -0600

    Se ve mas lindo asi

--- src/models/empleado.model.ts
+++ src/models/empleado.model.ts
@@ -7,27 +7,14 @@ const sequelize = connection._sequelizeConnection;
 export default class Empleado extends Model { }
 
 Empleado.init({
-    id_empleado: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    nombre: {
-        type: DataTypes.STRING
-    },
-    correo: {
-        type: DataTypes.STRING
-    },
-    departamento: {
-        type: DataTypes.STRING
-    },
-    telefono: {
-        type: DataTypes.STRING
-    },
+    id_empleado: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    nombre: { type: DataTypes.STRING },
+    correo: { type: DataTypes.STRING },
+    departamento: { type: DataTypes.STRING },
+    telefono: { type: DataTypes.STRING },
    }, 
    {
     sequelize,
-    modelName: 'Empleado',
     tableName: 'Empleados',  // Especifica el nombre de la tabla manualmente ('empleados')
     freezeTableName: true,   // Evitamos la pluralización automática
     timestamps: false,
